64e202f1a152c1439a7e84c9890fab5eec88b77c
Ethan Brandle <ethan@brandle.design>
2024-11-07 02:38:32 +0000

    Chapter 2 lessons

--- constants.py
+++ constants.py
@@ -1,9 +1,16 @@
-#Set width of screen in px
+# Set width of screen in px
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
-#set dimensions and number of asteroids
+# set dimensions and number of asteroids
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+# set player values
+PLAYER_RADIUS = 20
+
+# SET MOVEMENT VARIABLES
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # import libraries
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,14 +16,21 @@ def main():
 	while True:
 		# sets fill color to black 
 		screen.fill(000000)
-		
+		player.draw(screen)		
+
 		# tells the program to refresh the screen
+		player.update(dt)
 		pygame.display.flip()
 		
+		# sets up a tick system for managing the clock
+		dt = clock.tick(60) / 1000
+
 		# Check if user has closed window, exit game loop if so
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
+		
 
 if __name__ == "__main__":
 	main()
